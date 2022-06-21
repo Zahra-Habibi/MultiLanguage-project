dc87740048f39b42e7bf672b741be26f1d64a914
DELL 5440 i5 <habibi.zahra.ku@gmail.com>
2022-06-21 08:51:47 -0700

    datalayer added

---
+++ MultiLanguage.DataLayer/Entities/ApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multipisus.DataLayer.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        public string Department { get; set; }
+        public bool IsDelete { get; set; }
+        public bool IsActive { get; set; }
+
+        public string UserImg { get; set; }
+        public DateTime RegisterDate { get; set; } = DateTime.UtcNow;
+
+    }
+}
